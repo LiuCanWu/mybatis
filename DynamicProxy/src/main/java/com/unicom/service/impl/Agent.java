449eb75090f6b81cb26acf3401ef678d8e11a5fa
刘阳 <liuy2590@chinaunicom.cn>
2022-09-06 22:01:57 +0800

    second commit

--- DynamicProxy/src/main/java/com/unicom/service/impl/Agent.java
+++ DynamicProxy/src/main/java/com/unicom/service/impl/Agent.java
@@ -5,6 +5,7 @@ import com.unicom.service.Service;
 public class Agent implements Service {
     @Override
     public void sing() {
+        System.out.println("我是职业经纪人！");
         System.out.println("预约时间");
         System.out.println("预约场地");
         SuperLiu liu = new SuperLiu();
